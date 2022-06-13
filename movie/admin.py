dc57d3844c1b04aca7cb59c6700fa7925e2721e4
Md Nazmul Hasan <farhapartex@gmail.com>
2022-06-13 18:07:34 +0600

    added movie table

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from movie import models
+
 
 # Register your models here.
+
+@admin.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "year", "imdb_id", "type", "is_favorite")
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth import models as dj_auth_models
+from core import base_models
 # Create your models here.
+
+
+class Movie(base_models.BaseAbstractModel):
+    title = models.CharField(max_length=300)
+    year = models.CharField(max_length=5)
+    imdb_id = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    poster = models.CharField(max_length=500)
+    is_favorite = models.BooleanField(default=False)
+    is_watched = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
